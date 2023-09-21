d66754d1deb70ad3fa8a6e5c08a4ca848e9ea89d
bibhuti <144211577+bibhutiswain9@users.noreply.github.com>
2023-09-21 23:46:38 +0530

    add chat initial process

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Icon name="add" size={30} />
+      </View>
+    </SafeAreaView>
+  );
+}
